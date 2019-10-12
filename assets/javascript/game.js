153cdb68cd4034b3fcf1686eebff5a0411bce9d3
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-12 00:38:40 -0700

    fixed a bug where selecting one character gives two

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,49 @@
 $(document).ready(function () {
     var firstCharacter = $('#hero1');
     var secondCharacter = $('#hero2');
-    var thirdCharacter = $('#heros3');
+    var thirdCharacter = $('#hero-last');
     var fightBox = $('.fightBox');
     var firstVillain = $('#vill1');
     var characters = $('.heroine');
 
     firstCharacter.on('click', function () {
-        if (fightBox.children(secondCharacter).length === 1 || fightBox.children(secondCharacter).length === 2) {
+        if (fightBox.children(secondCharacter).length >= 1) {
             characters.append(secondCharacter);
-            fightBox.append(firstCharacter);
-        } else if (fightBox.children(thirdCharacter).length === 1 || fightBox.children(thirdCharacter).length === 2) {
+            fightBox.prepend(firstCharacter);
+        }
+        if (fightBox.children(thirdCharacter).length >= 0) {
             characters.append(thirdCharacter);
-            fightBox.append(firstCharacter);
+            fightBox.prepend(firstCharacter);
         } else {
-            fightBox.append(firstCharacter);
+            fightBox.prepend(firstCharacter);
         }
     });
 
     secondCharacter.on('click', function () {
-        fightBox.append(secondCharacter);
+        if (fightBox.children(firstCharacter).length >= 1) {
+            characters.append(firstCharacter);
+            fightBox.prepend(secondCharacter);
+        }
+        if (fightBox.children(thirdCharacter).length >= 0) {
+            characters.append(thirdCharacter);
+            fightBox.prepend(secondCharacter);
+        } else {
+            fightBox.prepend(secondCharacter);
+        }
     });
+
+    thirdCharacter.on('click', function () {
+        if (firstCharacter.parent(fightBox).length === 1) {
+            characters.append(firstCharacter);
+            fightBox.prepend(thirdCharacter);
+        } else if (fightBox.children(secondCharacter).length >= 1) {
+            characters.append(secondCharacter);
+            fightBox.prepend(thirdCharacter);
+        } else {
+            fightBox.prepend(thirdCharacter);
+        }
+    });
+
 
     firstVillain.on('click', function () {
         fightBox.append(firstVillain);
