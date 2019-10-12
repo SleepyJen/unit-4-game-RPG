bb4808568f49404f5aae0522bbc8c7e60f0c647e
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-11 23:54:58 -0700

    made characters switchable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,26 @@
 
 $(document).ready(function () {
     var firstCharacter = $('#hero1');
+    var secondCharacter = $('#hero2');
+    var thirdCharacter = $('#heros3');
     var fightBox = $('.fightBox');
     var firstVillain = $('#vill1');
     var characters = $('.heroine');
 
     firstCharacter.on('click', function () {
-        fightBox.append(firstCharacter);
+        if (fightBox.children(secondCharacter).length === 1 || fightBox.children(secondCharacter).length === 2) {
+            characters.append(secondCharacter);
+            fightBox.append(firstCharacter);
+        } else if (fightBox.children(thirdCharacter).length === 1 || fightBox.children(thirdCharacter).length === 2) {
+            characters.append(thirdCharacter);
+            fightBox.append(firstCharacter);
+        } else {
+            fightBox.append(firstCharacter);
+        }
+    });
+
+    secondCharacter.on('click', function () {
+        fightBox.append(secondCharacter);
     });
 
     firstVillain.on('click', function () {
