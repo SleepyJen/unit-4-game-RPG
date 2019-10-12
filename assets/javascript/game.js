bdb8a49a70fff5d412ff23755f3546b83c8b90cf
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-12 09:52:43 -0700

    made the character box clickable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,20 @@ $(document).ready(function () {
     var firstCharacter = $('#hero1');
     var secondCharacter = $('#hero2');
     var thirdCharacter = $('#hero-last');
+
+    var characters = $('.heroine');
+    var villains = $('.villains');
     var fightBox = $('.fightBox');
     var firstVillain = $('#vill1');
-    var characters = $('.heroine');
+    var secondVillain = $('#vill2');
+    var thirdVillain = $('#vill3');
 
     firstCharacter.on('click', function () {
         if (fightBox.children(secondCharacter).length >= 1) {
             characters.append(secondCharacter);
             fightBox.prepend(firstCharacter);
         }
-        if (fightBox.children(thirdCharacter).length >= 0) {
+        if (fightBox.children(thirdCharacter).length >= 1) {
             characters.append(thirdCharacter);
             fightBox.prepend(firstCharacter);
         } else {
@@ -34,10 +38,11 @@ $(document).ready(function () {
     });
 
     thirdCharacter.on('click', function () {
-        if (firstCharacter.parent(fightBox).length === 1) {
+        if (fightBox.children(firstCharacter).length >= 1) {
             characters.append(firstCharacter);
             fightBox.prepend(thirdCharacter);
-        } else if (fightBox.children(secondCharacter).length >= 1) {
+        }
+        if (fightBox.children(secondCharacter).length >= 1) {
             characters.append(secondCharacter);
             fightBox.prepend(thirdCharacter);
         } else {
@@ -45,9 +50,43 @@ $(document).ready(function () {
         }
     });
 
-
+    //villains
     firstVillain.on('click', function () {
-        fightBox.append(firstVillain);
+        if (fightBox.children(secondVillain).length >= 1) {
+            villains.append(secondVillain);
+            fightBox.prepend(firstVillain);
+        }
+        if (fightBox.children(thirdVillain).length >= 1) {
+            villains.append(thirdVillain);
+            fightBox.prepend(firstVillain);
+        } else {
+            fightBox.append(firstVillain);
+        }
     });
 
+    secondVillain.on('click', function () {
+        if (fightBox.children(firstVillain).length >= 1) {
+            villains.append(firstVillain);
+            fightBox.prepend(secondVillain);
+        }
+        if (fightBox.children(thirdVillain).length >= 1) {
+            villains.append(thirdVillain);
+            fightBox.prepend(secondVillain);
+        } else {
+            fightBox.append(secondVillain);
+        }
+    });
+
+    thirdVillain.on('click', function () {
+        if (fightBox.children(secondVillain).length >= 1) {
+            villains.append(secondVillain);
+            fightBox.prepend(thirdVillain);
+        }
+        if (fightBox.children(secondVillain).length >= 1) {
+            villains.append(secondVillain);
+            fightBox.prepend(thirdVillain);
+        } else {
+            fightBox.append(thirdVillain);
+        }
+    });
 });
